44eadbf7d1a58436dba56954d26b602ad69ebd4c
ahMarrone <agustinhmarrone@gmail.com>
2016-07-07 14:23:11 -0300

    ShuntingYard retorna Queue, en vez de String

--- src/trec/org/terrier/querying/rpn/ShuntingYard.java
+++ src/trec/org/terrier/querying/rpn/ShuntingYard.java
@@ -13,37 +13,37 @@ public class ShuntingYard{
   }
 
   // Converts a infix string into postfix notation (boolean strings)
-  public static String toPostfix(String inString){
+  public static Queue toPostfix(String inString){
     Deque<String> tmpStack = new ArrayDeque<String>(); // algorithm stack
-    StringBuilder outString = new StringBuilder(); // out string. Queue as string.
-    //Queue<String> outQueue = new LinkedList<String>();
+    //StringBuilder outString = new StringBuilder(); // out string. Queue as string.
+    Queue<String> outQueue = new LinkedList<String>();
 
     for (String token : inString.split("\\s")) {
       if (ShuntingYard.isBooleanOperator(token)){
         while (ShuntingYard.isBooleanOperator((String)tmpStack.peek())){ // top of stack is operator?
-            outString.append(tmpStack.pop()).append(" ");
-            //outQueue.add(tmpStack.pop());
+            //outString.append(tmpStack.pop()).append(" ");
+            outQueue.add(tmpStack.pop());
         }
         tmpStack.push(token);
       } else if (token.equals(")")){
         while (!tmpStack.peek().equals("(")){
-          outString.append(tmpStack.pop()).append(" ");
-          //outQueue.add(tmpStack.pop());
+          //outString.append(tmpStack.pop()).append(" ");
+          outQueue.add(tmpStack.pop());
         }
         tmpStack.pop();
       } else if (token.equals("(")){
         tmpStack.push(token);
       } else { // raw token. vocabulary term
-        outString.append(token).append(" ");
-        //outQueue.add(token);
+        //outString.append(token).append(" ");
+        outQueue.add(token);
       }
     }
     while (!tmpStack.isEmpty()){
-      outString.append(tmpStack.pop()).append(" ");
-      //outQueue.add(tmpStack.pop());
+      //outString.append(tmpStack.pop()).append(" ");
+      outQueue.add(tmpStack.pop());
     }
-    return outString.toString();
-    //return outQueue;
+    //return outString.toString();
+    return outQueue;
   }
 
 
